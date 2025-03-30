ae37bf66434345dd746ac97f99f9b045efa9f922
yogeshgaur001 <gauryogesh2003@gmail.com>
2025-03-30 16:57:41 +0530

    Add ingredients Data

---
+++ src/data/ingredients.ts
@@ -0,0 +1,6 @@
+export const ingredients = [
+  { name: "Cheese", price: 1.5, image: "/public/cheese.jpg" },
+  { name: "Pepperoni", price: 2.0, image: "/public/pepperoni.jpg" },
+  { name: "Mushrooms", price: 1.2, image: "/public/mushroom.jpg" },
+  { name: "Olives", price: 1.0, image: "/public/olives.jpg" },
+];
